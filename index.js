53950b77bdb24b497eb2f1d3c18c0e40e0bf0ba0
Zainab Fatima <162745461+zainabfatima67@users.noreply.github.com>
2024-04-03 01:50:29 -0700

    created number guessing game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randaomNumer = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Kindly guess a random number from 1 to 6",
+    }
+]);
+if (answer.userGuessedNumber === randaomNumer) {
+    console.log("Congratulations You Guessed The Right Number");
+}
+else {
+    console.log("Sorry You Guesed The Wrong Number");
+}
